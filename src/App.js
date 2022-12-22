f446cf947e3ca57558085c22768212bbddaa376f
Nicola <nicolar.bell@gmail.com>
2022-12-22 12:43:13 +0000

    app structure in react and jsx to build component containers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import "./style.css";
 
-//this is jsx
-
 function App() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="Today I Learned Logo" />
-        <h1>Today I Learned</h1>
-      </div>
-      <button className="btn btn-large btn-open">Share a fact</button>
-    </header>
+    <>
+      {/* HEADER */}
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Today I Learned Logo" />
+          <h1>Today I Learned</h1>
+        </div>
+        <button className="btn btn-large btn-open">Share a fact</button>
+      </header>
+      <NewFactForm />
+
+      <main class="main">
+        <CategoryFilter />
+        <FactList />
+      </main>
+    </>
   );
+}
+
+function NewFactForm() {
+  return <form className="fact-form">Fact form</form>;
+}
+
+// Category filters
+
+function CategoryFilter() {
+  return <aside>Category filter</aside>;
+}
+
+function FactList() {
+  return <section>factlist</section>;
 }
 
 export default App;
